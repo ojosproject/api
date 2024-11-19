dfbe97abe374481a68c8cd5dbc3e614abe16a45e
jasonbm2 <jasonbradymartin@gmail.com>
2024-11-19 13:45:51 -0800

    chore: remove unnecessary dependencies

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ load_dotenv()
 
 app = Flask(__name__)
 
-TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
-TWILIO_AUTH_SID = os.getenv("TWILIO_AUTH_SID")
-TWILIO_PHONE_NUMBER = os.getenv("TWILIO_PHONE_NUMBER")
+# TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
+# TWILIO_AUTH_SID = os.getenv("TWILIO_AUTH_SID")
+# TWILIO_PHONE_NUMBER = os.getenv("TWILIO_PHONE_NUMBER")
 
-twilio_client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_SID)
+# twilio_client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_SID)
 
 
 @app.route("/send_sms", methods=["POST"])
@@ -36,4 +36,4 @@ def send_sms():
             return jsonify({"error": str(e)}), 500
     """
     # UNCOMMENT WHEN TWILIO INTEGRATION/VERIFICATION IS CONFIRMED
-    return jsonify({"status": "success", "sid": "success message"}), 200
+    return jsonify({"status": "success", "sid": 1234}), 200
